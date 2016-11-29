15242ad10ef028c4b43920ba900f098e6a03aa23
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-28 21:10:31 -0500

    clase prgramada por Fabian Ayala 2

---
+++ Col3/src/m_eva/sumita.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m_eva;
+
+/**
+ *
+ * @author usuario
+ */
+public class sumita {
+    
+    public int a;
+    public int b;
+    
+    //multi
+    
+}
