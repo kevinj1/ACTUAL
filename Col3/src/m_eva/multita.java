3256bb75f41f13f1671823112ca05215a767e5ea
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-28 21:21:43 -0500

    Clase de Kevin LOjan1

--- Col3/src/m_eva/multita.java
+++ Col3/src/m_eva/multita.java
@@ -5,10 +5,43 @@
  */
 package m_eva;
 
+import java.util.Locale;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author usuario
  */
 public class multita {
     public int a1;
+    public int a2;
+
+    public multita(int a1, int a2) {
+        this.a1 = a1;
+        this.a2 = a2;
+    }
+
+    public int getA1() {
+        return a1;
+    }
+
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+
+    public int getA2() {
+        return a2;
+    }
+
+    public void setA2(int a2) {
+        this.a2 = a2;
+    }
+    public void calcular(){
+        int alma;
+         alma = a1+a2;
+         JOptionPane.showMessageDialog(null, alma);
+                 
+                 
+                
+    }
 }
